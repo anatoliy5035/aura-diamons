ba699652ad92dfc1f8db70e6ca16c57c965fb6ee
anatoliy5035 <senator5035@yandex.ru>
2016-07-27 03:06:13 +0300

    add two sections

---
+++ assets/js/include.js
@@ -0,0 +1,25 @@
+
+jQuery(document).ready(function($){
+    //$(this).on('click.slick',function(){
+    //    if($('.slider .slide-2').hasClass('slick-active')) {
+    //        $('.slider').addClass('js-slide-2-bg')
+    //    }
+    //    else {
+    //        $('.slider').removeClass('js-slide-2-bg')
+    //    }
+    //
+    //});
+
+    $('.slider').slick(
+        {
+            dots: true,
+        }
+    );
+});
+
+
+
+
+
+
+
---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    autoprefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    rigger = require('gulp-rigger'),
+    cssmin = require('gulp-minify-css'),
+    connect = require('gulp-connect'),
+    browserSync = require("browser-sync"),
+    reload = browserSync.reload;
+
+var path = {
+    build: {
+        css: '',
+        img: 'build/img/',
+        icons: 'build/img/icons',
+        fonts: 'build/fonts/'
+    },
+    src: {
+        html: 'src/*.html',
+        js: 'src/js/**/*.js',
+        sass: 'assets/scss/style.scss',
+        img: 'src/img/**/*.*',
+        //video: 'src/video/**/*.*',
+        svg: 'src/svg/*.svg',
+        icons: 'src/img/icons/*.png',
+        fonts: 'src/fonts/**/*.*'
+    },
+    watch: {
+        sass: 'assets/scss/**/*.*',
+        img: 'src/img/**/*.*'
+        //video: 'src/video/**/*.*',
+    },
+    clean: './build'
+};
+
+//server
+gulp.task('server', function () {
+    connect.server({
+        livereload: true
+    });
+});
+
+
+//sass
+gulp.task('sass:build', function () {
+    return gulp.src(path.src.sass)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(path.build.css));
+});
+
+
+gulp.task('watch', function(){
+    watch([path.watch.sass], function(event, cb) {
+        gulp.start('sass:build');
+    });
+});
+
+
+gulp.task('default', ['sass:build', 'server', 'watch']);
