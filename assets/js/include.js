1f00fb7d5f5a236098beef9aa302b1965960627e
anatoliy5035 <senator5035@yandex.ru>
2016-07-28 12:00:22 +0300

    last changes

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,20 +1,40 @@
 
 jQuery(document).ready(function($){
-    //$(this).on('click.slick',function(){
-    //    if($('.slider .slide-2').hasClass('slick-active')) {
-    //        $('.slider').addClass('js-slide-2-bg')
-    //    }
-    //    else {
-    //        $('.slider').removeClass('js-slide-2-bg')
-    //    }
-    //
-    //});
+    //init slick slider
+    $('.slider').slick({
+            dots: true
+        });
+    $('.testimonials__slider').slick({
+            dots: true
+        });
+    //init validate
+    $(".registration-form").validate({
+        rules: {
 
-    $('.slider').slick(
-        {
-            dots: true,
         }
-    );
+    });
+    $.validator.messages.required = '';
+    $.validator.messages.email = '';
+    $.validator.messages.digits = '';
+
+    $("#phone").mask('(099)-099-99-99');
+
+    //modal popup
+    $('.js-open-modal').on('click',function(e){
+        $('.modal').css('display','block');
+        setTimeout(function(){
+            $(".modal").addClass('fade');
+        }, 10);
+        $('body').css('overflow','hidden');
+    });
+    $('.modal-close').on('click',function(e){
+        $('.modal').removeClass('fade');
+        setTimeout(function(){
+            $('.modal').css('display','none');
+            $('body').css('overflow','visible');
+        }, 200);
+
+    })
 });
 
 
